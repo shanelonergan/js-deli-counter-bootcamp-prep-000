887adb564b5f96aa030d0236c2f69c4effa2a68c
Shane Lonergan <sptlonergan@gmail.com>
2019-07-03 15:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function nowServing(katzDeliLine) {
     return (`Currently serving ${katzDeliLine.shift()}.`);
   }
 }
+
+function currentLine(array) {
+  if (array.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    for (var i = 0; i < array.length; i++) {
+      console.log (`The line is currently: ${i}. ${katzDeliLine[i]}`);
+    }
+  }
+}
